152af4077ee5af80c77d813299f89411fcbedebe
yonghk423 <yonghk423423@gmail.com>
2022-06-21 19:24:56 +0900

    styled-c setup

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
 import webpack from 'webpack';
-import 'webpack-dev-server';
+import 'webpack-dev-server'; //개발용 서버
 import path from 'path';
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'; 
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'; //hot-reloading
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -40,28 +40,24 @@ const config: webpack.Configuration = {
             '@babel/preset-react',
             '@babel/preset-typescript',
           ],
-                    env: {
-                        development: {
-                            plugins: [require.resolve('react-refresh/babel')],
-                        },    
-                    },
-                },
-                exclude: path.join(__dirname, 'node_modules'),
+        env: {
+            development: {
+            plugins: [require.resolve('react-refresh/babel')],
+                },    
             },
-            {
-                test: /\.css?$/,
-                use: ['style-loader', 'css-loader'],
-            },
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+            
+    },
+    {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+    },
         ],
     },
     plugins: [
-        new ForkTsCheckerWebpackPlugin({
-      async: false,
-    //   eslint: {
-    //     files: "./src/**/*",
-    //   },
-    }),
-    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+        new ForkTsCheckerWebpackPlugin({ async: false }),
+        new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
     ],
     output : {
         path: path.join(__dirname, 'dist'),
@@ -78,12 +74,11 @@ const config: webpack.Configuration = {
 if (isDevelopment && config.plugins) {
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
   config.plugins.push(new ReactRefreshWebpackPlugin());
-//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
+//config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
 }
 if (!isDevelopment && config.plugins) {
   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
-//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+//config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
 }
-
 
 export default config;

daf7f6045223909412a7685abae70a49eae030a5
yonghk423 <yonghk423423@gmail.com>
2022-07-05 19:20:53 +0900

    reset

---
+++ src/Routes/Tv.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+const Test = styled.div`
+color: red
+`;
+
+const Tv = () => {
+    return (
+        <Test>왜 안보이지 </Test>
+    )
+}
+
+export default Tv;

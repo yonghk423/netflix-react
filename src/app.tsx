d11e3b67a4f80fa979778ce1fdf195382ede173e
yonghk423 <yonghk423423@gmail.com>
2022-07-05 19:26:38 +0900

    reset

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
+
 const App = () => {
     return (
-        <>
-        <div>test</div>
-        </>
+        <BrowserRouter>
+            <Header/>            
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="movies/:id" element={< Home />} /> 
+                <Route path="tv" element={<Tv />}></Route>
+                <Route path="search" element={<Search />}></Route>                
+            </Routes>
+        </BrowserRouter>
     )
 }
 
